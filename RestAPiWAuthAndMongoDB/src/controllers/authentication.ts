7a54fc0cd9b57f9f844e162928710f7ce3bc02b7
yagizharman <yagizharman4biz@gmail.com>
2023-06-08 17:37:47 +0300

    namefix

--- RestAPiWAuthAndMongoDB/src/controllers/authentication.ts
+++ RestAPiWAuthAndMongoDB/src/controllers/authentication.ts
@@ -28,7 +28,7 @@ export const login = async(req:express.Request,res:express.Response)=>{
   
       await user.save();
   
-      res.cookie('ANTONIO-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+      res.cookie('YAGIZ-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
   
       return res.status(200).json(user).end();
     } catch (error) {
